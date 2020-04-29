139d89018219e4c767eef59564f05161454d4966
Patulea <FP073407@cerner.net>
2020-04-29 14:53:47 +0300

    Module 6 redefined

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
-  constructor(props) {
-    super(props);
-    debugger;
-  }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
@@ -12,28 +9,26 @@ class QuizQuestion extends Component {
   }
   render() {
     return (
-      <div>
-        <main>
-          <section>
-            <p>{this.props.quiz_question.instruction_text}</p>
-          </section>
-          <section className="buttons">
-            <ul>
-              {this.props.quiz_question.answer_options.map(
-                (answer_options, index) => {
-                  return (
-                    <QuizQuestion
-                      key={index}
-                      button_text={answer_options}
-                      clickHandler={this.handleClick.bind(this)}
-                    ></QuizQuestion>
-                  );
-                }
-              )}
-            </ul>
-          </section>
-        </main>
-      </div>
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => {
+                return (
+                  <QuizQuestionButton
+                    key={index}
+                    button_text={answer_option}
+                    clickHandler={this.handleClick.bind(this)}
+                  />
+                );
+              }
+            )}
+          </ul>
+        </section>
+      </main>
     );
   }
 }
